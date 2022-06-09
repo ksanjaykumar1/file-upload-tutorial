405f6e8d7f2b714d40bcec87c843ef8c6544e6d8
sanjay <ksanjayk@protonmail.com>
2022-06-10 00:26:41 +0530

    express-async-errors package needed to handdler errors

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## gotcha 
+
+don't forget to install this package of else errorhandling functions won't work
+
+require("express-async-errors");
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
 const Logger = require("./logger/logger");
@@ -7,12 +9,18 @@ const logger = Logger.getLogger("./app.js");
 // local module imports
 const connectDB = require("./db/connect");
 const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const auth = require("./routes/auth");
 
 const port = process.env.PORT || 5000;
 app.use(express.json({ extended: false }));
 
+app.use("/api/v1/auth", auth);
+
 // custom middleware
-app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 const start = async () => {
   try {
