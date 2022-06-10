accf9dfdff2071b748f8579f60acb95d3d069ee7
sanjay <ksanjayk@protonmail.com>
2022-06-10 14:44:50 +0530

    controller functionality

--- controllers/product.js
+++ controllers/product.js
@@ -1,15 +1,18 @@
 const { StatusCodes } = require("http-status-codes");
 const Product = require("../models/Product");
+const Logger = require("../logger/logger");
+const logger = Logger.getLogger("./controllers/product");
 
 const createProduct = async (req, res) => {
+  logger.info(`Create product body`, req.body);
   const product = await Product.create(req.body);
-  res.status(StatusCodes.OK).json({ product });
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "all products" });
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
-
 
 module.exports = {
   createProduct,
--- controllers/upload.js
+++ controllers/upload.js
@@ -1,5 +1,18 @@
+const { StatusCodes } = require("http-status-codes");
+const path = require("path");
+const Logger = require("../logger/logger");
+const logger = Logger.getLogger("./controllers/uploads");
 const uploadProductImage = async (req, res) => {
-  res.send("Images uploaded");
+  //   logger.info(JSON.stringify(req.files));
+  const productImage = req.files.image;
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  return res.status(StatusCodes.OK).json({
+    image: { src: `/uploads/${productImage.name}` },
+  });
 };
 
 module.exports = {
