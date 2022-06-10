cee673cf950a05635a4a8ef93bd0ba38e700e05f
sanjay <ksanjayk@protonmail.com>
2022-06-10 18:25:09 +0530

    local file upload

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-
-## gotcha 
+## gotcha
 
 don't forget to install this package of else errorhandling functions won't work
 
 require("express-async-errors");
+
+express-fileupload
+
+use express-fileupload to access file in req.body
--- controllers/upload.js
+++ controllers/upload.js
@@ -1,10 +1,27 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const { BadRequest } = require("../errors");
 const Logger = require("../logger/logger");
 const logger = Logger.getLogger("./controllers/uploads");
 const uploadProductImage = async (req, res) => {
   //   logger.info(JSON.stringify(req.files));
+
+  //check file exists
+  if (!req.files) {
+    throw new BadRequest("No Image uploaded");
+  }
   const productImage = req.files.image;
+
+  // check file type
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new BadRequest("Please upload a image");
+  }
+
+  //check file size
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new BadRequest("File is larger than 1MB");
+  }
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${productImage.name}`
